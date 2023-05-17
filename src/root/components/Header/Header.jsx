98d9f3c40ce90c3be9e09214aa701d24067ca7c9
Jarol <jarolsala@gmail.com>
2023-05-16 19:42:23 -0600

    Inicio Header

---
+++ src/root/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Reacth from 'react';
+import './Header.css'; 
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <h1 className="header-title">Este es el encabezado</h1>
+    </header>
+  );
+};
+
+export default Header;
