ba562d04015d630154860aff705672eec36829ed
Jarol <jarolsala@gmail.com>
2023-06-14 17:46:23 -0600

    Arreglo de compatibilidad con Vercel

--- src/root/components/Body/Body.tsx
+++ src/root/components/Body/Body.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Slider from "react-slick";
 import Carousel from "./Carousel";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
